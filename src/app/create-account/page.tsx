7d351039f44f064ccaef547438f79b5c6c3b31b3
Enikő Valkó <eaaenva@students.eaaa.dk>
2025-05-05 21:23:44 +0200

    burger

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -1,22 +1,34 @@
-
 import CreateUser from "@/components/create-user";
+import Image from "next/image";
 
 export default function CreateAccountPage() {
   return (
-    <div className="container mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      {/* Card Flex */}
-      <div className="flex flex-col">
-        <CreateUser />
-        <div className="flex w-full justify-center items-center mt-4 gap-2">
-          <p className="text-white">Already have an account?</p>
-          <a
-            href="/"
-            className="font-semibold text-blue-300 hover:cursor-pointer hover:text-blue-600"
-          >
-            Sign in
-          </a>
+    <div className="flex max-h-screen h-screen ">
+      {/* Left Section: Create Account */}
+      <div className="flex flex-1 justify-center items-center">
+        <div className="w-full max-w-md">
+          <CreateUser />
+          <div className="flex justify-center items-center mt-4 gap-2">
+            <p className="text-gray-700">Already have an account?</p>
+            <a
+              href="/"
+              className="font-semibold text-blue-500 hover:cursor-pointer hover:text-blue-700"
+            >
+              Sign in
+            </a>
+          </div>
         </div>
-        {/* <CreateUser/> */}
+      </div>
+
+      {/* Right Section: Image */}
+      <div className="flex-1 relative max-h-screen hidden sm:block">
+        <Image
+          src="/images/Hamburger-bro.svg"
+          alt="Placeholder"
+          width={450}
+          height={450}
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
       </div>
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,34 @@
-
 import Login from "../components/login";
+import Image from "next/image";
 
 export default function Entry() {
-
   return (
-    <div className="container mx-auto absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-      {/* Card Flex */}
-      <div className="flex flex-col">
-        <Login />
-        <div className="flex w-full justify-center items-center mt-4 gap-2">
-          <p className="text-white">First time here?</p>
-          <a
-            href="/create-account"
-            className="font-semibold text-blue-300 hover:cursor-pointer hover:text-blue-600"
-          >
-            Create an account
-          </a>
+    <div className="flex max-h-screen h-screen">
+      {/* Left Section: Login */}
+      <div className="flex flex-1 justify-center items-center">
+        <div className="w-full max-w-md">
+          <Login />
+          <div className="flex justify-center items-center mt-4 gap-2">
+            <p className="text-gray-700">First time here?</p>
+            <a
+              href="/create-account"
+              className="font-semibold text-blue-500 hover:cursor-pointer hover:text-blue-700"
+            >
+              Create an account
+            </a>
+          </div>
         </div>
+      </div>
+
+      {/* Right Section: Image */}
+      <div className="flex-1 relative  max-h-screen  hidden sm:block">
+        <Image
+          src="/images/Hamburger-bro.svg"
+          alt="Placeholder"
+          width={450}
+          height={450}
+          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        />
       </div>
     </div>
   );
